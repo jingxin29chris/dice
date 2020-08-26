e7932df26404a467d085044ea971a4c245855c6f
xin <xjing@scu.edu>
2020-08-26 16:29:47 -0700

    First commit

--- bet.rb
+++ bet.rb
@@ -33,20 +33,18 @@ end
 
 post '/login' do
       @person = Person.get(params[:username])
-       p "****************************"
-       p params[:username]
       if(@person !=nil && @person.password == params[:password])
          session[:login] = true
          session[:name] = params[:username]
-         session[:message] = "you have successful login"
-         erb :person_page
+         session[:message] = "You have successful login"
+         redirect "/bet/#{@person.username}"
        else
           session[:message] = "username and password do not match, please try again"
           redirect '/'
         end
  end
 
-
+#post router to deal with bet
 post '/bet/:username' do
    person = Person.get(params[:username])
    stake = params[:money].to_i
@@ -62,7 +60,7 @@ post '/bet/:username' do
    redirect "/bet/#{person.username}"
 end
 
-
+#show certain username account infor
 get '/bet/:username' do
    @person = Person.get(params[:username])
     
@@ -74,7 +72,7 @@ get '/bet/logout/:username' do
  person = Person.get(params[:username])
  session[:login] = nil
  session[:name] = nil
- session[:message] = "you have successfully logout"
+ session[:message] = "You have successfully logout"
 if person.totalWin !=nil
  person.totalWin += (session[:win] || 0).to_i
 else
@@ -101,16 +99,30 @@ end
 
    #save_session(:lost, 1000)
    #save_session(:win, 1000)
+#use this method to record the lost and win during each login
 def save_session(won_lost, money)
    count = (session[won_lost] || 0).to_i
    count += money
    session[won_lost] = count
 end
-
+#method to calcuate the profit for each session
 def profit(win, lost)
    count = (session[win] || 0).to_i
    count1 = (session[lost] || 0).to_i
    count - count1
+end
+get '/new' do
+      erb :person_form
+end
+
+post '/new' do
+     client = Person.create(params[:client])
+redirect "/login"
+
+ end
+# handle invalid url
+not_found do
+    erb :notFound
 end
 
 
---
+++ readme.txt
@@ -0,0 +1,4 @@
+Please use the username: xin password :ruby
+to login 
+Or you would sign up your own username and password, then login
+the website address: https://xinjinghw2.herokuapp.com/
